969e2849f987f0cbac2cf934a4a92fa732015044
MahoMeshitsuka <ohlololmaho@gmail.com>
2023-07-29 15:12:00 +0900

    Slash 0729

---
+++ Source/Slash/Public/Characters/SlashCharacter.h
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CharacterTypes.h"
+#include "SlashCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UGroomComponent;
+class AItem;
+class UAnimMontage;
+
+UCLASS()
+class SLASH_API ASlashCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ASlashCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+protected:
+	virtual void BeginPlay() override;
+
+	/**
+	* Callbacks for input
+	*/
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void Turn(float Value);
+	void LookUp(float Value);
+	void EKeyPressed();
+	void Attack();
+
+	/**
+	* Play montage functions
+	*/
+	void PlayAttackMontage();
+
+	UFUNCTION(BlueprintCallable)
+		void AttackEnd();
+	bool CanAttack();
+private:
+
+	ECharacterState CharacterState = ECharacterState::ECS_Unequipped;
+
+	UPROPERTY(BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+		EActionState ActionState = EActionState::EAS_Unoccupied;
+
+	UPROPERTY(VisibleAnywhere)
+		USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere)
+		UCameraComponent* ViewCamera;
+
+	UPROPERTY(VisibleAnywhere, Category = Hair)
+		UGroomComponent* Hair;
+
+	UPROPERTY(VisibleAnywhere, Category = Hair)
+		UGroomComponent* Eyebrows;
+
+	UPROPERTY(VisibleInstanceOnly)
+		AItem* OverlappingItem;
+
+	/**
+	* Animation montages
+	*/
+	UPROPERTY(EditDefaultsOnly, Category = Montages)
+		UAnimMontage* AttackMontage;
+
+public:
+	FORCEINLINE void SetOverlappingItem(AItem* Item) { OverlappingItem = Item; }
+	FORCEINLINE ECharacterState GetCharacterState() const { return CharacterState; }
+};
